32017ce4e73d243c7fd13f1c5cc62d4be8e4f2d5
Adrian Ciobanita <paul.ciobanita@rockwellcollins.com>
2020-01-21 07:56:06 +0000

    Add main app, migration, sql capabilities.

--- views/__init__.py
+++ views/__init__.py
@@ -2,7 +2,14 @@
 # author: 'ACIOBANI'
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from microblog.configuration import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from views import routes
+from views import routes, models
